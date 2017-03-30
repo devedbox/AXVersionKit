ec004961763028853bcb5ec9f344f87123c8a47e
devedbox <devedbox@gmail.com>
2017-03-30 16:53:08 +0800

    Initial Commit

---
+++ AXVersionKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXVersionKit
+//
+//  Created by devedbox on 2017/3/30.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXVersionKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXVersionKit
+//
+//  Created by devedbox on 2017/3/30.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
