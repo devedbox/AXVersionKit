c314cebb953973597dd030a95dfe3db794befaa3
devedbox <devedbox@gmail.com>
2017-03-30 16:57:59 +0800

    Create AVKVersion module.

---
+++ AXVersionKit/Classes/AVKVersion.h
@@ -0,0 +1,13 @@
+//
+//  AVKVersion.h
+//  AXVersionKit
+//
+//  Created by devedbox on 2017/3/30.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVKVersion : NSObject
+
+@end
---
+++ AXVersionKit/Classes/AVKVersion.m
@@ -0,0 +1,13 @@
+//
+//  AVKVersion.m
+//  AXVersionKit
+//
+//  Created by devedbox on 2017/3/30.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import "AVKVersion.h"
+
+@implementation AVKVersion
+
+@end
